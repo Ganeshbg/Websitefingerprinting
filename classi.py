a39c0438dd1da42d134d7e5d7900929332c54108
Ganeshbg <ganesh.byrandurgagopinath@colorado.edu>
2017-10-20 22:17:36 -0600

    Update classi.py

--- classi.py
+++ classi.py
@@ -1,3 +1,3 @@
 import os
 import subprocess
-x = subprocess.call(['sudo','tcpdump', '-vv', 'host stackoverflow.com','-w','test1.pcap'])
+x = subprocess.call(['sudo','tcpdump', '-vv', 'host stackoverflow.com','-w','test1.pcap','-Z','root'])
